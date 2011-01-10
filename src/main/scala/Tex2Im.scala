e1df35d73b6d9ef1c21cf024a32a6c6d35f34c5a
Yuvi Masory <ymasory@gmail.com>
2011-01-09 22:19:54 -0500

    add working image maker

--- src/main/scala/Tex2Im.scala
+++ src/main/scala/Tex2Im.scala
@@ -4,7 +4,7 @@ import java.io._
 
 import StringUtils.LF
 
-class Tex2Im(fontSize: Int, texCode: String, headers: List[String]) {
+class Tex2Im(fontSize: Int, texCode: String) {
 
   val FileName = "temp"
   val Dvi = ".dvi"
@@ -13,38 +13,41 @@ class Tex2Im(fontSize: Int, texCode: String, headers: List[String]) {
 
   lazy val texDoc = {
     val builder = new StringBuilder()
-    builder append "\\documentclass[" + fontSize.toString + "pt]{article}"
+    builder append "\\documentclass[" + fontSize.toString + "pt]{article}" + LF
     builder append "\\usepackage{color}" + LF
+    builder append "\\usepackage{amsmath}" + LF
+    builder append "\\usepackage{amssymb}" + LF
     builder append "\\usepackage[dvips]{graphicx}" + LF
     builder append "\\pagestyle{empty}" + LF
-
-    builder append LF
-    headers foreach {h => builder append (h + LF)}
-    builder append LF
-
     builder append "\\pagecolor{white}" + LF
     builder append "\\begin{document}" + LF
-    builder append "{\\color{black}" + LF
-    builder append "\\begin{eqnarray*}" + LF
+    builder append "\\color{black}" + LF
 
     builder append LF
-    builder append texCode + LF
+    builder append "$" + texCode + "$" + LF
     builder append LF
-
-    builder append "\\end{eqnarray*}}" + LF
+ 
     builder append "\\end{document}" + LF
-
     builder.toString
   }
 
 
-  def makeImage {
+  def makeImage() {
 
     val out = new BufferedWriter(new FileWriter(FileName + Tex))
     out.write(texDoc)
     out.close()
 
     Runtime.getRuntime.exec(("latex -interaction=batchmode " + FileName + Tex).split(" "))
-    Runtime.getRuntime.exec(("dvips -o " + FileName + Eps + " -E " + FileName + Dvi).split(" "))
+    def toEps() = Runtime.getRuntime.exec(("dvips -o " + FileName + Eps + " -E " + FileName + Dvi).split(" "))
+    toEps(); toEps();
+  }
+}
+
+object Tex2Im {
+
+  def main(args: Array[String]) {
+    val maker = new Tex2Im(12, """x \in X \forall z \in \mathbb{N}""")
+    maker.makeImage()
   }
 }
